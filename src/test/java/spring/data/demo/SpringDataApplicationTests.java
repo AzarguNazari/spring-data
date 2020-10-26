4e3e8f77ce59d2f718fd9515f81ed2fca29a12a9
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-10-26 16:35:55 +0100

    initla commit

---
+++ src/test/java/spring/data/demo/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
